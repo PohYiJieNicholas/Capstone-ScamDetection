7f4b021f565cb4ee3f1ac53b9a1fd784e24a6a31
Poh Yi Jie Nicholas <2101720@sit.singaporetech.edu.sg>
2024-06-28 00:05:43 +0800

    Change urls host

--- app/src/main/java/com/example/scamdetection/apiCall/RetrofitInstance.kt
+++ app/src/main/java/com/example/scamdetection/apiCall/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     val api: ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://192.168.68.52:8080/")  // Use 10.0.2.2 for localhost in the emulator
+            .baseUrl("https://scam-detection-zf4ey5dwtq-as.a.run.app/")  // Use 10.0.2.2 for localhost in the emulator
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
